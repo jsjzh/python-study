b103f672f433bfdabad3985f686e57aab1993371
jsjzh <kimimi_king@163.com>
2026-08-05 18:02:11 +0800

    feat: reg demo

--- src/python_study/__init__.py
+++ src/python_study/__init__.py
@@ -4,6 +4,7 @@ import os
 from os import path
 import pickle
 import random
+import re
 import subprocess
 import time
 
@@ -113,7 +114,19 @@ def subProcessTask():
 
 
 def regDemo():
-    pass
+    print('----- re.match(r"\\d", "1123") -----', re.match(r"^\d+", "1123"))
+    print('----- re.split(r"\\s+", "a b c  d e") -----', re.split(r"\s+", "a b c  d e"))
+    result = re.match(r"^(\d{4})-(\d{3,8})$", "0570-00000000")
+    if result:
+        print("----- result.groups() -----", result.groups())
+        print("----- result.group(0) -----", result.group(0))
+        print("----- result.group(1) -----", result.group(1))
+        print("----- result.group(2) -----", result.group(2))
+    regPhone = re.compile(r"(^\d{11}$)")
+    print(
+        "----- regPhone.match('11111111111').groups() -----",
+        regPhone.match("11111111111").groups(),
+    )
 
 
 def main() -> None:
@@ -127,4 +140,5 @@ def main() -> None:
     # processTasks()
     # subProcessTask()
     ## subProcessTaskCommunicate()
+    # regDemo()
     pass
