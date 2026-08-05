fe508897f2280934c8a4430d18c0e0890f036439
jinzhehao <jinzhehao@souche.com>
2026-08-05 13:37:22 +0800

    feat: path file demo

--- src/python_study/__init__.py
+++ src/python_study/__init__.py
@@ -1,10 +1,18 @@
-from importlib import resources
+import os
+from os import path
+
+base = path.join(os.getcwd(), "src", "python_study")
 
 
-def readFile():
-    with resources.files("python_study").joinpath("files/index.md").open() as f:
-        print(f.read())
+def _getProjectPath(paths: list[str]) -> str:
+    return path.join(base, *paths)
+
+
+def writeFile():
+    filePath = _getProjectPath(paths=["files", "index.md"])
+    with open(filePath, "a+") as f:
+        f.write("\nhello world")
 
 
 def main() -> None:
-    readFile()
+    pass
