9ab1556a6225bb6c6af06c0fcec7314346b17d31
jsjzh <kimimi_king@163.com>
2026-08-05 21:33:49 +0800

    feat: hashlib demo

--- src/python_study/__init__.py
+++ src/python_study/__init__.py
@@ -2,6 +2,7 @@ from python_study.modules import (
     base64Demo,
     collectionsDemo,
     dateTimeDemo,
+    hashDemo,
     regDemo,
     processDemo,
     serializeDemo,
@@ -14,5 +15,6 @@ def main() -> None:
     # regDemo.main()
     # dateTimeDemo.main()
     # collectionsDemo.main()
-    base64Demo.main()
+    # base64Demo.main()
+    # hashDemo.main()
     pass
---
+++ src/python_study/modules/hashDemo.py
@@ -0,0 +1,32 @@
+import hashlib
+
+sb = b"a"
+
+
+def main():
+    print("----- hashlib.md5(sb).hexdigest() -----", hashlib.md5(sb).hexdigest())
+    print("----- hashlib.sha1(sb).hexdigest() -----", hashlib.sha1(sb).hexdigest())
+    print("----- hashlib.sha224(sb).hexdigest() -----", hashlib.sha224(sb).hexdigest())
+    print("----- hashlib.sha256(sb).hexdigest() -----", hashlib.sha256(sb).hexdigest())
+    print("----- hashlib.sha384(sb).hexdigest() -----", hashlib.sha384(sb).hexdigest())
+    print("----- hashlib.sha512(sb).hexdigest() -----", hashlib.sha512(sb).hexdigest())
+    print(
+        "----- hashlib.sha3_224(sb).hexdigest() -----", hashlib.sha3_224(sb).hexdigest()
+    )
+    print(
+        "----- hashlib.sha3_256(sb).hexdigest() -----", hashlib.sha3_256(sb).hexdigest()
+    )
+    print(
+        "----- hashlib.sha3_384(sb).hexdigest() -----", hashlib.sha3_384(sb).hexdigest()
+    )
+    print(
+        "----- hashlib.sha3_512(sb).hexdigest() -----", hashlib.sha3_512(sb).hexdigest()
+    )
+    print(
+        "----- hashlib.shake_128(sb).hexdigest() -----",
+        hashlib.shake_128(sb).hexdigest(length=10),
+    )
+    print(
+        "----- hashlib.shake_256(sb).hexdigest() -----",
+        hashlib.shake_256(sb).hexdigest(length=10),
+    )
