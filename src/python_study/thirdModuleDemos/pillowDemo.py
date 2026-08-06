deccd956b408625bae23276199f3f99d30ae36d2
jsjzh <kimimi_king@163.com>
2026-08-06 11:41:30 +0800

    feat: pillow demo

--- src/python_study/thirdModuleDemos/pillowDemo.py
+++ src/python_study/thirdModuleDemos/pillowDemo.py
@@ -1,4 +1,6 @@
-from PIL import Image, ImageFilter
+import random
+
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
 
 from python_study.utils import getAssetsPath
 
@@ -15,8 +17,41 @@ def blurImg():
     return img.filter(ImageFilter.BLUR)
 
 
+# 随机字母:
+def rndChar():
+    return chr(random.randint(65, 90))
+
+
+# 随机颜色1:
+def rndColor():
+    return (random.randint(64, 255), random.randint(64, 255), random.randint(64, 255))
+
+
+# 随机颜色2:
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127), random.randint(32, 127))
+
+
+def drawImg():
+    w = 240
+    h = 60
+    img = Image.new("RGB", (w, h), (255, 255, 255))
+    font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 36)
+    draw = ImageDraw.Draw(img)
+
+    for x in range(w):
+        for y in range(h):
+            draw.point((x, y), fill=rndColor())
+
+    for t in range(4):
+        draw.text(xy=(60 * t + 12, 5), text=rndChar(), font=font, fill=rndColor2())
+
+    return img.filter(ImageFilter.BLUR)
+
+
 def main() -> None:
-    thumbnailImg().save(getAssetsPath(["car-thumb.png"]))
-    blurImg().save(getAssetsPath(["car-blur.png"]))
+    # thumbnailImg().save(getAssetsPath(["car-thumb.png"]))
+    # blurImg().save(getAssetsPath(["car-blur.png"]))
+    drawImg().save(getAssetsPath(["draw.png"]))
     # img.show("car")
     pass
