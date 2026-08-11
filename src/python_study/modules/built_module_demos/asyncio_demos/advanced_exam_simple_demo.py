8eb6cdcc32d04539642eb84bcd9a09d4d9459341
jsjzh <kimimi_king@163.com>
2026-08-11 18:03:01 +0800

    feat: step

--- src/python_study/modules/built_module_demos/asyncio_demos/advanced_exam_simple_demo.py
+++ src/python_study/modules/built_module_demos/asyncio_demos/advanced_exam_simple_demo.py
@@ -1,4 +1,16 @@
 import asyncio
+from collections.abc import Awaitable, Callable, Coroutine
+from typing import Any, ParamSpec, TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+# def retry_async(
+#     max_retries: int = 3, base_delay: float = 0.5, timeout: float | None = None
+# ) -> Callable[[Callable[P, Awaitable[R]]], Callable[P, Awaitable[R]]]:
+#     async def wrapper(*arg: R, **kwarg: R) -> None:
+#         pass
 
 
 async def run() -> None:
