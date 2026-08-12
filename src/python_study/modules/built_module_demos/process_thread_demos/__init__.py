5f3628101ef8443754153ea6250e6a2c45dddaf2
jsjzh <kimimi_king@163.com>
2026-08-12 18:10:36 +0800

    feat: process demo supplement

--- src/python_study/modules/built_module_demos/process_thread_demos/__init__.py
+++ src/python_study/modules/built_module_demos/process_thread_demos/__init__.py
@@ -2,10 +2,12 @@ from python_study.modules.built_module_demos.process_thread_demos import (
     all_def_demo,
     cpu_busy_demo,
     download_exam_demo,
+    process_demo,
 )
 
 
 def main() -> None:
     # all_def_demo.main()
     # download_exam_demo.main()
-    cpu_busy_demo.main()
+    # cpu_busy_demo.main()
+    process_demo.main()
---
+++ src/python_study/modules/built_module_demos/process_thread_demos/process_demo.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import os
+
+count: int = 0
+
+
+def output_pid() -> None:
+    global count
+    print(f"----- child start count: {count} -----")
+    count = 999
+    print(f"----- child end count: {count} -----")
+
+
+def main() -> None:
+    global count
+    multiprocessing.set_start_method("fork")
+
+    print(f"----- parent start count: {count} -----")
+
+    process = multiprocessing.Process(target=output_pid)
+    count = 42
+    process.start()
+    process.join()
+
+    print(f"----- parent end count: {count} -----")
